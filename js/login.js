5af01db5cbf33af8fcd2979eb0745b10633e033c
cenk-idris <114963355+cenk-idris@users.noreply.github.com>
2023-08-09 14:10:02 +0300

    add graphQL

---
+++ js/login.js
@@ -0,0 +1,36 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const chart = document.getElementById("pie-chart");
+
+    // Convert credentials to base64
+    const base64Credentials = btoa(username + ':' + password);
+
+    try {
+        // Make the POST request to the signin endpoint
+        const response = await fetch('https://01.gritlab.ax/api/auth/signin', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic ' + base64Credentials,
+            },
+        });
+
+        if (response.status === 200) {
+            chart.style.display = "none";
+            const data = await response.json();
+            localStorage.setItem("jwt", data);
+            location.reload(); // Save JWT to local storage
+            console.log(data)
+            window.location.href = '/selfQL/profile.html'; // Redirect to the profile page
+        } else {
+            let alertDiv = document.getElementById('alert-response')
+            alertDiv.textContent = 'Invalid credentials, please try again';
+
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+        document.getElementById('errorMessage').textContent = 'An error occurred. Please try again later.';
+    }
+});
---
+++ js/script.js
@@ -0,0 +1,85 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    console.log("DOMContentLoaded event listener");
+    const chart = document.getElementById("pie-chart");
+    const jwt = localStorage.getItem("jwt");
+    console.log(jwt)
+
+    if (jwt == undefined && window.location.pathname === "/selfQL/profile.html") {
+        window.location.pathname = "/selfQL/"
+    }
+    if (localStorage.getItem("skills") != undefined) {
+        chart.style.display = "none";
+        const technicalSkills = getTechnicalSkills(JSON.parse(localStorage.getItem("skills")))
+        const data = [
+            {
+                values: [],
+                labels: [],
+                type: 'pie',
+            },
+        ];
+        technicalSkills.forEach((item) => {
+            data[0].values.push(item.amount);
+            data[0].labels.push(item.skill);
+        });
+
+        console.log(data);
+
+        var layout = {
+            title: 'Technical Skills',
+            height: 400,
+            width: 500
+        };
+        let img_jpg = document.getElementById('jpg-export');
+        console.log(img_jpg);
+        Plotly.newPlot('pie-chart', data, layout, { staticPlot: true }).then(
+            function(gd)
+            {
+                Plotly.toImage(gd,{height:400,width:500})
+                    .then(
+                        function(url)
+                        {
+                            img_jpg.src = url;
+                        }
+                    )
+            });
+    }
+});
+
+
+function getTechnicalSkills(data) {
+    const skills = [];
+    data.forEach((skill) => {
+        if (skill.type.startsWith("skill_")) {
+            const existingSkill = skills.find(
+                (s) => s.skill === skill.type.slice(6)
+            );
+            if (existingSkill) {
+                existingSkill.amount += skill.amount;
+            } else {
+                skills.push({
+                    skill: skill.type.slice(6),
+                    amount: skill.amount,
+                });
+            }
+        }
+    });
+    // Arrays to store the separated skills
+    const technicalSkills = [];
+    // Separate the skills into respective arrays
+    skills.forEach(skill => {
+        if (
+            skill.skill === "prog" ||
+            skill.skill === "tcp" ||
+            skill.skill === "algo" ||
+            skill.skill === "game" ||
+            skill.skill === "back-end" ||
+            skill.skill === "front-end" ||
+            skill.skill === "sys-admin"
+        ) {
+            technicalSkills.push(skill);
+        }
+    });
+    console.log("Technical Skills:", technicalSkills);
+    return technicalSkills;
+
+}
